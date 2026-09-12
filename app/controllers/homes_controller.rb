71c66a279679278a17e42b5446a17ac8d39e2942
Ubuntu <ubuntu@ip-172-31-30-38.ap-northeast-1.compute.internal>
2026-09-12 01:43:05 +0000

    [add]ログアウトの追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,8 @@
 class HomesController < ApplicationController
+ allow_unauthenticated_access 
   def top
+  end
+
+  def about
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # ユーザー登録（サインアップ）のためのルーティングを追加
   resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
-  
   resource :session
   resources :passwords, param: :token
     root to: "homes#top"
@@ -10,7 +9,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+get 'homes/about', to: 'homes#about', as: 'about'
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
